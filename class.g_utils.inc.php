ea74deef6654d9d77f93538aa4519f05337804f0
gergo314 <gergo314@gmail.com>
2019-01-17 11:05:01 +0100

    Update class.g_utils.inc.php
    Added log function

--- class.g_utils.inc.php
+++ class.g_utils.inc.php
@@ -40,7 +40,33 @@ class g_utils {
   function yellow($str) {
       return "\033[33m".$str."\033[0m";
   }
-
+  function log($msg,$type = 'ERROR') {
+    $return = true;
+    $dir = __DIR__ .'/log';
+    $message = "";
+    if ($type == 'INLINE') {
+        $return = "<pre>";
+        $return .= var_export($msg,true);
+        $return .= "</pre>";
+    }
+    if ($type != 'INLINE') {
+        if (!is_dir($dir)) {
+            mkdir($dir);
+        }
+        $date = date('Y-m-d');
+        $time = date('H:i:s');
+        if (is_array($msg)) {
+            $message = var_export($msg,true);
+        }
+        else {
+            $message = $msg;
+        }
+        $message = "[" . $date . " " . $time . "]" . "\t[" . $type . "]\t" . $message . "\n";
+        $logfile = $dir."/".strtolower($type).".log";
+        file_put_contents($logfile,$message,FILE_APPEND);
+    }
+    return $return;
+  }
 }
 
 ?>
